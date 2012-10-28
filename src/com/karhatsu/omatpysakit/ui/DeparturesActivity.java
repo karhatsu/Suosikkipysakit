a541f66dda4a654d3f52b5147f27f917341efde7
Henri Karhatsu <henri@karhatsu.com>
2012-10-28 16:25:50 +0200

    progress dialog to departures view

--- src/com/karhatsu/omatpysakit/ui/DeparturesActivity.java
+++ src/com/karhatsu/omatpysakit/ui/DeparturesActivity.java
@@ -1,15 +1,19 @@
 package com.karhatsu.omatpysakit.ui;
 
 import android.app.ListActivity;
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.widget.ListView;
 
+import com.karhatsu.omatpysakit.R;
 import com.karhatsu.omatpysakit.datasource.OnStopRequestReady;
 import com.karhatsu.omatpysakit.datasource.StopRequest;
 import com.karhatsu.omatpysakit.domain.Stop;
 
 public class DeparturesActivity extends ListActivity implements
 		OnStopRequestReady {
+
+	private ProgressDialog progressDialog;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -19,11 +23,22 @@ public class DeparturesActivity extends ListActivity implements
 
 	private void queryDepartures() {
 		int stopCode = getIntent().getIntExtra(Stop.CODE_KEY, -1);
+		showProgressDialog();
 		new StopRequest(this).execute(stopCode);
+	}
+
+	private void showProgressDialog() {
+		progressDialog = new ProgressDialog(this);
+		progressDialog.setCancelable(false);
+		progressDialog.setTitle(R.string.please_wait);
+		progressDialog.show();
 	}
 
 	@Override
 	public void notifyStopRequested(Stop stop) {
+		if (progressDialog != null) {
+			progressDialog.dismiss();
+		}
 		ListView departuresListView = getListView();
 		DepartureListAdapter adapter = new DepartureListAdapter(this,
 				stop.getDepartures());
