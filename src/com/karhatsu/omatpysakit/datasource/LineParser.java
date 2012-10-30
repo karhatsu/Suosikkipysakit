c9e00080db7e59db474cc8a3d4fe58ceb7f36985
Henri Karhatsu <henri@karhatsu.com>
2012-10-30 19:48:24 +0200

    line formatting fixed for non-Helsinki lines

--- src/com/karhatsu/omatpysakit/datasource/LineParser.java
+++ src/com/karhatsu/omatpysakit/datasource/LineParser.java
@@ -3,18 +3,30 @@ package com.karhatsu.omatpysakit.datasource;
 public class LineParser {
 
 	public String format(String lineCode) {
-		if (lineCode.startsWith("2")) {
-			return formatRegionalLine(lineCode);
+		if (isJoukoLine(lineCode)) {
+			return formatJoukoLine(lineCode);
 		}
-		return formatInternalLine(lineCode);
+		return formatLine(lineCode);
 	}
 
-	private String formatRegionalLine(String lineCode) {
-		return lineCode.substring(1, 5).trim();
+	private boolean isJoukoLine(String lineCode) {
+		return lineCode.substring(4, 5).equals("J");
 	}
 
-	private String formatInternalLine(String lineCode) {
-		return lineCode.substring(2, 5).trim();
+	private String formatJoukoLine(String lineCode) {
+		return lineCode.substring(2, 5);
 	}
 
+	private String formatLine(String lineCode) {
+		return stripLeadingZeros(lineCode.substring(1, 5).trim());
+	}
+
+	private String stripLeadingZeros(String lineCode) {
+		if (lineCode.startsWith("00")) {
+			return lineCode.substring(2, lineCode.length());
+		} else if (lineCode.startsWith("0")) {
+			return lineCode.substring(1, lineCode.length());
+		}
+		return lineCode;
+	}
 }
