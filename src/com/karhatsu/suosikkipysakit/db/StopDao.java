c0d649dcaadf878ddc3c0bf184d44a983c152c85
Henri Karhatsu <henri@karhatsu.com>
2012-11-17 14:27:22 +0200

    StopDao with static imports

--- src/com/karhatsu/suosikkipysakit/db/StopDao.java
+++ src/com/karhatsu/suosikkipysakit/db/StopDao.java
@@ -1,5 +1,11 @@
 package com.karhatsu.suosikkipysakit.db;
 
+import static android.provider.BaseColumns._ID;
+import static com.karhatsu.suosikkipysakit.db.OwnStopsContract.StopEntry.COLUMN_CODE;
+import static com.karhatsu.suosikkipysakit.db.OwnStopsContract.StopEntry.COLUMN_COORDINATES;
+import static com.karhatsu.suosikkipysakit.db.OwnStopsContract.StopEntry.COLUMN_NAME;
+import static com.karhatsu.suosikkipysakit.db.OwnStopsContract.StopEntry.COLUMN_NAME_BY_USER;
+import static com.karhatsu.suosikkipysakit.db.OwnStopsContract.StopEntry.TABLE_NAME;
 import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
@@ -20,32 +26,25 @@ public class StopDao {
 	public void save(Stop stop) {
 		SQLiteDatabase db = getWritableDatabase(context);
 		ContentValues values = new ContentValues();
-		values.put(OwnStopsContract.StopEntry.COLUMN_CODE, stop.getCode());
-		values.put(OwnStopsContract.StopEntry.COLUMN_NAME, stop.getName());
-		values.put(OwnStopsContract.StopEntry.COLUMN_NAME_BY_USER,
-				stop.getNameByUser());
-		values.put(OwnStopsContract.StopEntry.COLUMN_COORDINATES,
-				stop.getCoordinates());
+		values.put(COLUMN_CODE, stop.getCode());
+		values.put(COLUMN_NAME, stop.getName());
+		values.put(COLUMN_NAME_BY_USER, stop.getNameByUser());
+		values.put(COLUMN_COORDINATES, stop.getCoordinates());
 		db.insert(OwnStopsContract.StopEntry.TABLE_NAME, null, values);
 		db.close();
 	}
 
 	public void delete(long id) {
 		SQLiteDatabase db = getWritableDatabase(context);
-		db.delete(OwnStopsContract.StopEntry.TABLE_NAME,
-				OwnStopsContract.StopEntry._ID + "=?",
-				new String[] { String.valueOf(id) });
+		db.delete(TABLE_NAME, _ID + "=?", new String[] { String.valueOf(id) });
 		db.close();
 	}
 
 	public Cursor findAll() {
 		SQLiteDatabase db = dbHelper.getReadableDatabase();
-		String[] projection = { OwnStopsContract.StopEntry._ID,
-				OwnStopsContract.StopEntry.COLUMN_CODE,
-				OwnStopsContract.StopEntry.COLUMN_NAME_BY_USER };
-		String sortBy = OwnStopsContract.StopEntry.COLUMN_NAME_BY_USER;
-		return db.query(OwnStopsContract.StopEntry.TABLE_NAME, projection,
-				null, null, null, null, sortBy);
+		String[] projection = { _ID, COLUMN_CODE, COLUMN_NAME_BY_USER };
+		String sortBy = COLUMN_NAME_BY_USER;
+		return db.query(TABLE_NAME, projection, null, null, null, null, sortBy);
 	}
 
 	public void close() {
