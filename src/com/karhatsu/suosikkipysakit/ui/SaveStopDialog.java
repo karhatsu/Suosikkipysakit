ff60d62e6f7ac17f67e38160907639bf5a5ef433
Henri Karhatsu <henri@karhatsu.com>
2012-11-18 17:01:39 +0200

    SaveStopDialog extends AlertDialog

--- src/com/karhatsu/suosikkipysakit/ui/SaveStopDialog.java
+++ src/com/karhatsu/suosikkipysakit/ui/SaveStopDialog.java
@@ -12,13 +12,14 @@ import com.karhatsu.suosikkipysakit.R;
 import com.karhatsu.suosikkipysakit.db.StopDao;
 import com.karhatsu.suosikkipysakit.domain.Stop;
 
-public class SaveStopDialog {
+public class SaveStopDialog extends AlertDialog {
 
 	private final Activity activity;
 	private final boolean isNew;
 	private long id;
 
 	public SaveStopDialog(Activity activity, final long stopId) {
+		super(activity);
 		this.activity = activity;
 		this.id = stopId;
 		this.isNew = false;
@@ -27,43 +28,57 @@ public class SaveStopDialog {
 	}
 
 	public SaveStopDialog(Activity activity, final Stop stop) {
+		super(activity);
 		this.activity = activity;
 		this.isNew = true;
 		buildAndShowDialog(stop);
 	}
 
 	private void buildAndShowDialog(final Stop stop) {
-		AlertDialog.Builder builder = new AlertDialog.Builder(activity);
-		LayoutInflater inflater = activity.getLayoutInflater();
-		View view = inflater.inflate(R.layout.dialog_save_stop, null);
-		builder.setView(view);
-		builder.setTitle(R.string.dialog_save_stop_title);
-		EditText stopName = (EditText) view
-				.findViewById(R.id.dialog_save_stop_name);
-		setInitialValue(stop, stopName);
-		setPositiveButton(stop, builder, stopName);
-		setNegativeButton(builder);
-		AlertDialog dialog = builder.create();
-		dialog.show();
+		buildDialog(stop);
+		show();
 	}
 
-	private void setInitialValue(final Stop stop, EditText stopName) {
+	private void buildDialog(final Stop stop) {
+		LayoutInflater inflater = activity.getLayoutInflater();
+		View view = inflater.inflate(R.layout.dialog_save_stop, null);
+		setView(view);
+		setTitle(R.string.dialog_save_stop_title);
+		EditText stopNameField = (EditText) view
+				.findViewById(R.id.dialog_save_stop_name);
+		setInitialName(stop, stopNameField);
+		setButtons(stop, stopNameField);
+	}
+
+	private void setInitialName(final Stop stop, EditText stopName) {
 		String name = stop.getNameByUser() != null ? stop.getNameByUser()
 				: stop.getName();
 		stopName.setText(name);
 	}
 
-	private void setPositiveButton(final Stop stop,
-			AlertDialog.Builder builder, final EditText stopName) {
-		builder.setPositiveButton(R.string.dialog_save_stop_save,
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						stop.setNameByUser(stopName.getText().toString());
-						dialog.dismiss();
-						saveStopAndShowAll(stop);
-					}
-				});
+	private void setButtons(final Stop stop, EditText stopName) {
+		setButton(BUTTON_POSITIVE,
+				activity.getString(R.string.dialog_save_stop_save),
+				new SaveButtonListener(stopName, stop));
+		setButton(BUTTON_NEGATIVE, activity.getString(R.string.cancel),
+				new CancelButtonListener());
+	}
+
+	private class SaveButtonListener implements DialogInterface.OnClickListener {
+		private EditText stopName;
+		private Stop stop;
+
+		private SaveButtonListener(EditText stopName, Stop stop) {
+			this.stopName = stopName;
+			this.stop = stop;
+		}
+
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			stop.setNameByUser(stopName.getText().toString());
+			dialog.dismiss();
+			saveStopAndShowAll(stop);
+		}
 	}
 
 	private void saveStopAndShowAll(Stop stop) {
@@ -76,13 +91,11 @@ public class SaveStopDialog {
 		activity.startActivity(intent);
 	}
 
-	private void setNegativeButton(AlertDialog.Builder builder) {
-		builder.setNegativeButton(R.string.cancel,
-				new DialogInterface.OnClickListener() {
-					@Override
-					public void onClick(DialogInterface dialog, int which) {
-						dialog.dismiss();
-					}
-				});
+	private class CancelButtonListener implements
+			DialogInterface.OnClickListener {
+		@Override
+		public void onClick(DialogInterface dialog, int which) {
+			dialog.dismiss();
+		}
 	}
 }
