ddfa4d5e9025491d5baeaee5a83a4036b04d3cbf
Henri Karhatsu <henri@karhatsu.com>
2012-12-23 13:09:04 +0200

    highlight changed minutes for a while in departures view

--- src/com/karhatsu/suosikkipysakit/ui/DepartureListAdapter.java
+++ src/com/karhatsu/suosikkipysakit/ui/DepartureListAdapter.java
@@ -1,8 +1,11 @@
 package com.karhatsu.suosikkipysakit.ui;
 
+import java.util.Calendar;
 import java.util.List;
 
 import android.content.Context;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,10 +18,26 @@ import com.karhatsu.suosikkipysakit.domain.Departure;
 public class DepartureListAdapter extends ArrayAdapter<Departure> {
 
 	private List<Departure> departures;
+	private int previousMinutes = -1;
+	private boolean minutesJustChanged;
+	private ColorStateList defaultTextViewColors;
 
 	public DepartureListAdapter(Context context, List<Departure> departures) {
 		super(context, R.layout.list_item_departure, departures);
 		this.departures = departures;
+	}
+
+	@Override
+	public void notifyDataSetChanged() {
+		recognizeMinuteChanging();
+		super.notifyDataSetChanged();
+	}
+
+	private void recognizeMinuteChanging() {
+		int currentMinutes = Calendar.getInstance().get(Calendar.MINUTE);
+		minutesJustChanged = previousMinutes != -1
+				&& previousMinutes != currentMinutes;
+		previousMinutes = currentMinutes;
 	}
 
 	@Override
@@ -30,10 +49,12 @@ public class DepartureListAdapter extends ArrayAdapter<Departure> {
 			view = layoutInflater.inflate(R.layout.list_item_departure, null);
 		}
 		Departure departure = departures.get(position);
-		setText(view, R.id.departure_list_item_time, departure.getTime());
-		setText(view, R.id.departure_list_item_min, getMinutesToGo(departure));
-		setText(view, R.id.departure_list_item_line, departure.getLine());
-		setText(view, R.id.departure_list_item_end_stop, departure.getEndStop());
+		setText(view, R.id.departure_list_item_time, departure.getTime(), false);
+		setText(view, R.id.departure_list_item_min, getMinutesToGo(departure),
+				minutesJustChanged);
+		setText(view, R.id.departure_list_item_line, departure.getLine(), false);
+		setText(view, R.id.departure_list_item_end_stop,
+				departure.getEndStop(), false);
 		return view;
 	}
 
@@ -47,8 +68,26 @@ public class DepartureListAdapter extends ArrayAdapter<Departure> {
 		return String.valueOf(min);
 	}
 
-	private void setText(View view, int resourceId, String text) {
+	private void setText(View view, int resourceId, String text,
+			boolean highLight) {
 		TextView textView = (TextView) view.findViewById(resourceId);
+		defineDefaultColors(textView);
 		textView.setText(text);
+		setTextViewColor(highLight, textView);
+	}
+
+	private void setTextViewColor(boolean highLight, TextView textView) {
+		if (highLight) {
+			int highlightColor = Color.parseColor("#f1d243");
+			textView.setTextColor(highlightColor);
+		} else {
+			textView.setTextColor(defaultTextViewColors);
+		}
+	}
+
+	private void defineDefaultColors(TextView textView) {
+		if (defaultTextViewColors == null) {
+			defaultTextViewColors = textView.getTextColors();
+		}
 	}
 }
